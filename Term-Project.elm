0303c2c1a257958a1bbf8c0bf293b70428b1c369
caleianderson <caan9517@colorado.edu>
2018-04-30 20:35:01 -0600

    Add files via upload
    Enemies now die when shot, bullets disappear when they kill and enemy, and enemies slowly move towards the player.

--- Term-Project.elm
+++ Term-Project.elm
@@ -31,7 +31,7 @@ type alias Model = {
 initialModel = {
    playerPos = 94,
    bullets = [],
-   enemies = [0,1,2,3,4,5,6,7,8,9]
+   enemies = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19]
   }
 
 init : (Model, Cmd Msg)
@@ -44,29 +44,31 @@ update : Msg -> Model -> (Model, Cmd Msg)
 update msg model =
   case msg of
     Presses code ->
+      let newEnemies = List.map (\ x->x+1) (remove model.bullets model.enemies []) in
+      let newBullets = List.map (\ x->x-10) (removeBul model.bullets model.enemies []) in
       case (code, model.playerPos) of
         ('w', x)->
           if (x==0) || (x==1) || (x==2) || (x==3) || (x==4) || (x==5) || (x==6) || (x==7) || (x==8) || (x==9)then
-            ({playerPos = x, bullets = List.map (\ x->x-10) model.bullets, enemies = model.enemies},Cmd.none)
+            ({playerPos = x, bullets = newBullets, enemies = newEnemies},Cmd.none)
           else
-            ({playerPos = x-10, bullets = List.map (\ x->x-10) model.bullets, enemies = model.enemies},Cmd.none)
+            ({playerPos = x-10, bullets = newBullets, enemies = newEnemies},Cmd.none)
         ('a', x)->
           if (x==0) || (x==10) || (x==20) || (x==30) || (x==40)  || (x==50) || (x==60) || (x==70) || (x==80) || (x==90)then
-            ({playerPos = x, bullets = List.map (\ x->x-10) model.bullets, enemies = model.enemies},Cmd.none)
+            ({playerPos = x, bullets = newBullets, enemies = newEnemies},Cmd.none)
           else
-            ({playerPos = x-1, bullets = List.map (\ x->x-10) model.bullets, enemies = model.enemies},Cmd.none)
+            ({playerPos = x-1, bullets = newBullets, enemies = newEnemies},Cmd.none)
         ('s', x)->
           if (x==90) || (x==91) || (x==92) || (x==93) || (x==94)  || (x==95) || (x==96) || (x==97) || (x==98) || (x==99)then
-            ({playerPos = x, bullets = List.map (\ x->x-10) model.bullets, enemies = model.enemies},Cmd.none)
+            ({playerPos = x, bullets = newBullets, enemies = newEnemies},Cmd.none)
           else
-            ({playerPos = x+10, bullets = List.map (\ x->x-10) model.bullets, enemies = model.enemies},Cmd.none)
+            ({playerPos = x+10, bullets = newBullets, enemies = newEnemies},Cmd.none)
         ('d', x)->
           if (x==9) || (x==19) || (x==29) || (x==39) || (x==49) || (x==59) || (x==69) || (x==79) || (x==89) || (x==99) then
-            ({playerPos = x, bullets = List.map (\ x->x-10) model.bullets, enemies = model.enemies},Cmd.none)
+            ({playerPos = x, bullets = newBullets, enemies = newEnemies},Cmd.none)
           else
-            ({playerPos = x+1, bullets = List.map (\ x->x-10) model.bullets, enemies = model.enemies},Cmd.none)
+            ({playerPos = x+1, bullets = newBullets, enemies = newEnemies},Cmd.none)
         (' ',x)->
-            ({playerPos = x, bullets =  List.map (\ x->x-10) (List.append model.bullets [x]), enemies = model.enemies},Cmd.none)
+            ({playerPos = x, bullets =  List.append newBullets [x-10], enemies = newEnemies},Cmd.none)
 
         (_,_)-> (model,Cmd.none)
 
@@ -86,21 +88,16 @@ generateMap player bul bad tot count=
     (0,_,_,0,_) -> "⍋ "
     (_,_,_,0,_) -> "⎕ "
 
-
-    (x,[],0::t,y,9) -> String.append "⍒ <br> <br>" (generateMap (x-1) ([]) (List.map (\ x->x-1) t) (y-1) 0)
-    (x,[],0::t,y,z) -> String.append "⍒ " (generateMap (x-1) ([]) (List.map (\ x->x-1) t) (y-1) (z+1))
-
-    (x,0::t,[],y,9) -> String.append "۞ <br> <br>" (generateMap (x-1) (List.map (\ x->x-1) t) ([]) (y-1) 0)
-    (x,0::t,[],y,z) -> String.append "۞ " (generateMap (x-1) (List.map (\ x->x-1) t) ([]) (y-1) (z+1))
-    ---bullets can't be in the final row so the first case isn't nescasary
     (x,h1::t1,h2::t2,y,9) ->
       let test = h1==h2 in
       if (test && (h1==0)) then
         String.append "⎃ <br> <br>" (generateMap (x-1) (List.map (\ x->x-1) t1) (List.map (\ x->x-1) t2) (y-1) 0)
       else if (h1==0) then
         String.append "۞ <br> <br>" (generateMap (x-1) (List.map (\ x->x-1) t1) (List.map (\ x->x-1) bad) (y-1) 0)
-      else
+      else if (h2==0) then
         String.append "⍒ <br> <br>" (generateMap (x-1) (List.map (\ x->x-1) bul) (List.map (\ x->x-1) t2) (y-1) 0)
+      else
+        String.append "⎕ <br> <br>" (generateMap (x-1) (List.map (\ x->x-1) bul) (List.map (\ x->x-1) bad) (y-1) 0)
 
     (x,h1::t1,h2::t2,y,z) ->
         let test = h1==h2 in
@@ -108,14 +105,19 @@ generateMap player bul bad tot count=
           String.append "⎃ " (generateMap (x-1) (List.map (\ x->x-1) t1) (List.map (\ x->x-1) t2) (y-1) (z+1))
         else if (h1==0) then
           String.append "۞ " (generateMap (x-1) (List.map (\ x->x-1) t1) (List.map (\ x->x-1) bad) (y-1) (z+1))
-        else
+        else if (h2==0) then
           String.append "⍒ " (generateMap (x-1) (List.map (\ x->x-1) bul) (List.map (\ x->x-1) t2) (y-1) (z+1))
+        else
+          String.append "⎕ " (generateMap (x-1) (List.map (\ x->x-1) bul) (List.map (\ x->x-1) bad) (y-1) (z+1))
 
-  --enemies
+    (x,[],0::t,y,9) -> String.append "⍒ <br> <br>" (generateMap (x-1) ([]) (List.map (\ x->x-1) t) (y-1) 0)
+    (x,[],0::t,y,z) -> String.append "⍒ " (generateMap (x-1) ([]) (List.map (\ x->x-1) t) (y-1) (z+1))
 
+    (x,0::t,en,y,9) -> String.append "۞ <br> <br>" (generateMap (x-1) (List.map (\ x->x-1) t) (List.map (\ x->x-1) en) (y-1) 0)
+    (x,0::t,en,y,z) -> String.append "۞ " (generateMap (x-1) (List.map (\ x->x-1) t) (List.map (\ x->x-1) en) (y-1) (z+1))
 
- --old bullets
-
+    (x,[],h::t,y,9) -> String.append "⎕ <br> <br>" (generateMap (x-1) ([]) (List.map (\ x->x-1) bad) (y-1) 0)
+    (x,[],h::t,y,z) -> String.append "⎕ " (generateMap (x-1) ([]) (List.map (\ x->x-1) bad) (y-1) (z+1))
 
     (0,_,b,x,9) -> String.append "⍋ <br> <br>" (generateMap (-1) (List.map (\ x->x-1) bul) (b) (x-1) 0)
     (y,_,b,x,9) -> String.append "⎕ <br> <br>" (generateMap (y-1) (List.map (\ x->x-1) bul) (b) (x-1) 0)
@@ -134,18 +136,27 @@ bulletUpdate ls =
       else
         ls
 
+--returns new list of enemies with the shot ones removed
+remove: List Int -> List Int -> List Int -> List Int
+remove bul bad new=
+  case bad of
+    [] -> new
+    h::t ->
+      if (member h bul) then
+        remove bul t new
+      else
+        remove bul t (new++[h])
 
-iHateMaybes : Maybe Int -> Int
-iHateMaybes val =
-  case val of
-    Just x -> x
-    _ -> 0
-
-iHateMaybes2 : Maybe(List(Int)) -> List Int
-iHateMaybes2 val =
-  case val of
-    Just(x) -> x
-    _ -> []
+--returns new list of enemies with the shot ones removed
+removeBul: List Int -> List Int -> List Int -> List Int
+removeBul bul bad new=
+  case bul of
+    [] -> new
+    h::t ->
+      if (member h bad) then
+        removeBul t bad new
+      else
+        removeBul t bad (new++[h])
 
 -- VIEW
 view : Model -> Html Msg
